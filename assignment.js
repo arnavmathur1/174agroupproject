8aabee16e1e78bc7228a9345c6bd30d7c75a7be2
arnavmathur1 <arnav.mathur176@gmail.com>
2021-12-02 23:10:22 -0800

    rocket

--- assignment.js
+++ assignment.js
@@ -6,6 +6,108 @@ const {
 
 
 
+//from https://github.com/Robert-Lu/tiny-graphics-shadow_demo/blob/master/examples/obj-file-demo.js?fbclid=IwAR2t3SJ42QS89LvHg1X9yfPLjp80O1ThklxkqSnwqV6dS7D0jK27s0z6pBA
+export class Shape_From_File extends Shape {                                   // **Shape_From_File** is a versatile standalone Shape that imports
+                                                                               // all its arrays' data from an .obj 3D model file.
+    constructor(filename) {
+        super("position", "normal", "texture_coord");
+        // Begin downloading the mesh. Once that completes, return
+        // control to our parse_into_mesh function.
+        this.load_file(filename);
+    }
+
+    load_file(filename) {                             // Request the external file and wait for it to load.
+        // Failure mode:  Loads an empty shape.
+        return fetch(filename)
+            .then(response => {
+                if (response.ok) return Promise.resolve(response.text())
+                else return Promise.reject(response.status)
+            })
+            .then(obj_file_contents => this.parse_into_mesh(obj_file_contents))
+            .catch(error => {
+                this.copy_onto_graphics_card(this.gl);
+            })
+    }
+
+    parse_into_mesh(data) {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];
+        unpacked.norms = [];
+        unpacked.textures = [];
+        unpacked.hashindices = {};
+        unpacked.indices = [];
+        unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;
+        var NORMAL_RE = /^vn\s/;
+        var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;
+        var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            var elements = line.split(WHITESPACE_RE);
+            elements.shift();
+
+            if (VERTEX_RE.test(line)) verts.push.apply(verts, elements);
+            else if (NORMAL_RE.test(line)) vertNormals.push.apply(vertNormals, elements);
+            else if (TEXTURE_RE.test(line)) textures.push.apply(textures, elements);
+            else if (FACE_RE.test(line)) {
+                var quad = false;
+                for (var j = 0, eleLen = elements.length; j < eleLen; j++) {
+                    if (j === 3 && !quad) {
+                        j = 2;
+                        quad = true;
+                    }
+                    if (elements[j] in unpacked.hashindices)
+                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                    else {
+                        var vertex = elements[j].split('/');
+
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                        if (textures.length) {
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 0]);
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 1]);
+                        }
+
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 0]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 1]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 2]);
+
+                        unpacked.hashindices[elements[j]] = unpacked.index;
+                        unpacked.indices.push(unpacked.index);
+                        unpacked.index += 1;
+                    }
+                    if (j === 3 && quad) unpacked.indices.push(unpacked.hashindices[elements[0]]);
+                }
+            }
+        }
+        {
+            const {verts, norms, textures} = unpacked;
+            for (var j = 0; j < verts.length / 3; j++) {
+                this.arrays.position.push(vec3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
+                this.arrays.normal.push(vec3(norms[3 * j], norms[3 * j + 1], norms[3 * j + 2]));
+                this.arrays.texture_coord.push(vec(textures[2 * j], textures[2 * j + 1]));
+            }
+            this.indices = unpacked.indices;
+        }
+        this.normalize_positions(false);
+        this.ready = true;
+    }
+
+    draw(context, program_state, model_transform, material) {               // draw(): Same as always for shapes, but cancel all
+        // attempts to draw the shape before it loads:
+        if (this.ready)
+            super.draw(context, program_state, model_transform, material);
+    }
+}
+
 const origin = vec3(0, 0, 0);
 
 let origin_relative = vec3(0, 0, 0);
@@ -78,7 +180,9 @@ export class Assignment extends Scene {
         this.r_flag = 0;
         this.delay = 4;
         this.u = 12;
+    
 
+        
 
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
@@ -92,6 +196,7 @@ export class Assignment extends Scene {
             moon: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version()) (1),
             ground: new defs.Capped_Cylinder(80,80, [[0, 2], [0, 1]]),
             skybox_night: new defs.Subdivision_Sphere(4),
+            rocket: new Shape_From_File( "/assets/rocket.obj" ),
                          
             // TODO:  Fill in as many additional shape instances as needed in this key/value table.
             //        (Requirement 1)
@@ -248,6 +353,7 @@ export class Assignment extends Scene {
         {
         rocket_matrix = Mat4.identity();
         rocket_matrix = rocket_matrix.times(Mat4.translation(...origin_relative)).times(Mat4.translation(0,this.sproj(this.u,t-this.contact_time-this.delay),0));
+        rocket_matrix = rocket_matrix.times(Mat4.scale(.3,.3,.3));
         this.contact_complete = 1;
         //console.log(t-this.contact_time-this.delay)
 
@@ -255,6 +361,7 @@ export class Assignment extends Scene {
         else if(this.contact_complete==0)
         {
             rocket_matrix = rocket_matrix.times(Mat4.translation(...origin_relative));
+            rocket_matrix = rocket_matrix.times(Mat4.scale(.3,.3,.3));
         }
 
                  
@@ -290,7 +397,7 @@ export class Assignment extends Scene {
          if(this.rocket_contact==1){
          if (this.vproj(this.u, t-this.contact_time-this.delay)>0)
          {
-             this.shapes.sphere.draw(context, program_state, rocket_matrix, this.materials.matp3);
+             this.shapes.rocket.draw(context, program_state, rocket_matrix, this.materials.matp3);
              //this.flag=1;
          }
          else if(this.explosion_flag==0)
@@ -301,10 +408,9 @@ export class Assignment extends Scene {
          }
          }
          else{
-             this.shapes.sphere.draw(context, program_state, rocket_matrix, this.materials.matp3);
+             this.shapes.rocket.draw(context, program_state, rocket_matrix, this.materials.matp3);
          }
 
-        //this.shapes.sphere.draw(context, program_state, p3matrix, this.materials.matp3);
 
         this.shapes.cube.draw(context, program_state, cubetransform, this.materials.matp1);
 
@@ -359,12 +465,11 @@ export class Assignment extends Scene {
     
 
         //console.log(this.contact_time)
-        console.log(this.flag)
 
         if(this.flag==1){
         for(var i = 0; i < 300; i++){
             model_transform = Mat4.identity();
-            model_transform = model_transform.times(Mat4.translation(...origin_relative)).times(Mat4.translation(0,7.35,0)).times(Mat4.scale(1/40,1/40,1/40));;       
+            model_transform = model_transform.times(Mat4.translation(...origin_relative)).times(Mat4.translation(0,7.35,0)).times(Mat4.scale(1/30,1/30,1/30));;       
             this.sparks[i].position = (this.sparks[i].velocity.times(1)).plus(this.sparks[i].position);
             this.sparks[i].velocity = (this.sparks[i].acceleration.times(0.4*Math.random())).plus(this.sparks[i].velocity);
             this.sparks[i].velocity = (this.sparks[i].velocity).times(0.7)
